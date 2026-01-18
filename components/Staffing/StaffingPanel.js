8c9fdc6a0c8f6b030cffd9431f8c67780cf09feb
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2026-01-18 14:49:59 -0500

    Changed the scroll bar for staffing panel

--- components/Staffing/StaffingPanel.js
+++ components/Staffing/StaffingPanel.js
@@ -6,26 +6,29 @@ import { Badge } from "@/components/ui/badge";
 
 const StaffingPanel = async ({ searchParams }) => {
     await connectDB();
-
+  
     const sort = searchParams?.sort === "old" ? 1 : -1;
-
+  
     const staffings = await Staffing.find({})
-        .sort({ createdAt: sort })
-        .lean();
-
+      .sort({ createdAt: sort })
+      .lean();
+  
     return (
-        <div className="bg-surface">
-            <div className="border-b border-default">
-                <StaffingPanelHeader total={staffings.length} sort={sort} />
+        <div className="bg-surface h-full flex flex-col">
+            <div className="sticky top-0 z-10 border-b border-default">
+                <StaffingPanelHeader total={staffings.length} />
             </div>
-        
-            <div className="p-4 space-y-3">
-                {staffings.map((s) => (
-                    <StaffingCard key={s._id} staffing={s} />
-                ))}
+    
+            <div className="flex-1 overflow-y-auto">
+                <div className="space-y-3 p-4">
+                    {staffings.map((s) => (
+                        <StaffingCard key={s._id} staffing={s} />
+                    ))}
+                </div>
             </div>
         </div>
     );
-}
-
+};
+  
 export default StaffingPanel;
+  
